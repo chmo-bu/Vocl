89ddc92f5c36c667059644d824533784bd7e3b01
erojash <erojash@bu.edu>
2022-11-14 20:06:39 -0500

    fixed play button bug

---
+++ proto_redo/Assets/Scripts/Main.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+//using System.Windows.Forms.Timer;
+
+public class Main : MonoBehaviour
+{
+    public GameObject loadingObject;
+    public GameObject menuObject;
+    //public GameObject game;
+   // public GameObject button;
+    //public GameObject play_button
+   
+    private float timeLeft = 5.0f;
+
+    private bool switchDone = false;
+
+    //List<AsyncOperation> scenesToLoad = new List<AsyncOperation>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello world!");
+        HideMenu();
+       // HideGame();
+        //scenesToLoad.Add(SceneManager.LoadSceneAsync)
+        ShowLoadingScreen();
+    }
+
+/*
+    public void HideGame()
+    {
+        game.SetActive(false);
+    } */
+
+
+    public void HideMenu()
+    {
+        menuObject.SetActive(false);
+    }
+
+    public void ShowLoadingScreen()
+    {
+        loadingObject.SetActive(true);
+    }
+
+    public void switchToMenu()
+    {
+        loadingObject.SetActive(false);
+        menuObject.SetActive(true);
+       //button.SetActive(true);
+    }
+    
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if(timeLeft < 0 && switchDone == false)
+        {
+            switchToMenu();
+            switchDone = true;
+        }
+    }
+}
