ae8ddc77e325bcc008ef8318504ed7d7c18e23a3
Hernandez <erojash@bu.edu>
2022-11-30 20:17:11 -0500

    new dreamscape env, fruit game added, fix loading screen

---
+++ proto_redo/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public GameObject loadingObject;
+    public GameObject prompt;
+   // public GameObject menu;
+
+    
+    public void LoadScene(int sceneID)
+    {
+        //SceneManager.LoadScene(sceneID);
+        //StartCoroutine(LoadSceneAsync(sceneID));
+       // menu.SetActive(false);
+       // StartCoroutine(LoadScene(sceneID));
+       prompt.SetActive(false);
+        StartCoroutine(LoadSceneAsync(sceneID));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneID)
+    {
+       // loadingObject.SetActive(true);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+        loadingObject.SetActive(true);
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+
+       // loadingObject.SetActive(false);
+    }
+    
+}
