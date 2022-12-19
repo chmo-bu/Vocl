04c052ef5a66070430e07f8f3f65372475c28c21
erojash <erojash@bu.edu>
2022-12-18 23:15:25 -0700

    rabbit now moves to new destination

--- proto_redo/Assets/Scripts/MoveWithTerrain.cs
+++ proto_redo/Assets/Scripts/MoveWithTerrain.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveFruit : MonoBehaviour
+public class MoveWithTerrain : MonoBehaviour
 {
-    public float speed;
+    public float offset;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +14,7 @@ public class MoveFruit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position) + 4f, transform.position.z); 
+        transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position) + offset, transform.position.z); 
         //+ 1 * speed * Time.deltaTime);
 
     }
