cef06deb1a13cb6528aafa19ec98181a8decc6f9
erojash <erojash@bu.edu>
2023-02-23 16:11:18 -0500

    latest

---
+++ proto_redo/Assets/Scripts/ClapAudio.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClapAudio
+{
+    public static class Play
+    {
+        public static void playAudio()
+        {
+           // AudioSource audioSource = GetComponent<AudioSource>();
+            //audioSource.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+            //audioSource.Play();
+            Debug.Log(" ");
+        }
+    }
+}
