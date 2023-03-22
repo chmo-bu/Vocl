1d36682a273cf11fdd95d0db83fb0570d34e17cf
Charles <chmo@bu.edu>
2023-03-21 23:57:43 -0400

    change format of server

---
+++ server/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='VoclServer',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'matplotlib'
+    ],
+)
